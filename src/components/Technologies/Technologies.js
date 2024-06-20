170bf962a2cc1d57e382f0fb419f598caca70523
Atulbedwal <atulbedwal69@gmail.com>
2024-06-20 12:18:11 +0530

    upadte

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -9,7 +9,7 @@ const Technologies = () =>  (
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range a technologies in the web development world.
-      From Back-end To Design
+      From Front-end To Design
     </SectionText>
     <List>
       <ListItem>
@@ -20,7 +20,7 @@ const Technologies = () =>  (
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            React.js.
+            React.js, Next.js.
           </ListParagraph>
         </ListContainer>
       </ListItem>
@@ -32,7 +32,7 @@ const Technologies = () =>  (
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            Node, Express and MongoDB.
+            Nodejs, Express and MongoDB.
           </ListParagraph>
         </ListContainer>
       </ListItem>
